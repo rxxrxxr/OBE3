e2e5fce940e1efa4646a394daca36c7e188c11a4
SuCock <ksymin313@gmail.com>
2024-05-28 15:13:51 +0900

    https사용할려고 설정

--- onezo3/src/main/java/com/onezogreen/onezo3/Onezo3Application.java
+++ onezo3/src/main/java/com/onezogreen/onezo3/Onezo3Application.java
@@ -1,9 +1,12 @@
 package com.onezogreen.onezo3;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(servers = {@Server(url = "https://admin.hellomh.site")})
 @SpringBootApplication
 @MapperScan("com.onezogreen.onezo3")
 public class Onezo3Application {
