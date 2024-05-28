329c544b25bcb2074e7ee3bf5d3ee55116da0e89
DESKTOP-AGV6E1U\GoChanWoo <wnf2255@gmail.com>
2024-05-28 15:08:28 +0900

    240528 스웨거 추가

--- onezo3/src/main/java/com/onezogreen/onezo3/review/ReviewController.java
+++ onezo3/src/main/java/com/onezogreen/onezo3/review/ReviewController.java
@@ -1,5 +1,8 @@
 package com.onezogreen.onezo3.review;
 
+import com.onezogreen.onezo3.exception.BizException;
+import com.onezogreen.onezo3.exception.ErrorCode;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -15,15 +18,22 @@ public class ReviewController {
 
     public final ReviewService reviewService;
 
+    @Operation(summary = "리뷰,댓글,별점 조회",
+            description = "store_id를 param값으로 받아서 review, reply, star를 리스트로 조회"
+    )
     @GetMapping("/select/{store_id}")
-//    @Schema(hidden = true)
     public ResponseEntity<List<ReviewVo>> getReviewReply(@PathVariable Long store_id) {
         List<ReviewVo> reviewReply = reviewService.getReviewReply(store_id);
-        return ResponseEntity.ok(reviewReply);
+        if (reviewReply == null || reviewReply.isEmpty()) {
+            throw new BizException(ErrorCode.NOTSELECT);
+        }
 
+        return ResponseEntity.ok(reviewReply);
     }
 
-    // 댓글 달기 & 수정
+    @Operation(summary = "댓글 달기 & 수정",
+            description = "review_id를 param값으로 받아서 reply를 body값으로 보내면 성공여부를 알려줌"
+    )
     @PutMapping("/reply/update/{review_id}")
     public String addReply(@PathVariable Long review_id, @RequestBody ReviewVo reviewVo) {
         reviewService.AddReply(review_id, reviewVo);
@@ -31,7 +41,9 @@ public class ReviewController {
         return "댓글 달기(수정)성공";
     }
 
-    // 댓글 삭제
+    @Operation(summary = "댓글 삭제",
+            description = "review_id를 parm값으로 받아서 replyDelete_yn 값을 기본값인 N 에서 Y로 바꿔줌"
+    )
     @PutMapping("/reply/delete/{review_id}")
     public String deleteReply(@PathVariable Long review_id, ReviewVo reviewVo) {
         reviewService.DeleteReply(review_id, reviewVo);
@@ -39,7 +51,9 @@ public class ReviewController {
         return "댓글 삭제 성공";
     }
 
-    // 리뷰삭제
+    @Operation(summary = "댓글 삭제",
+            description = "review_id를 parm값으로 받아서 reviewDelete_yn 값을 기본값인 N 에서 Y로 바꿔줌"
+    )
     @PutMapping("/delete/{review_id}")
     public String deleteReview(@PathVariable Long review_id, ReviewVo reviewVo) {
         reviewService.DeleteReview(review_id, reviewVo);
