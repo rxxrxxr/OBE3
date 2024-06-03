25712d036072ec04c0b99ab0e86a83728295d450
DESKTOP-JNNCO6N\user <sinjihoon6@gmail.com>
2024-06-03 15:21:10 +0900

    order,sale 수정

--- onezo3/src/main/java/com/onezogreen/onezo3/sales/SalesService.java
+++ onezo3/src/main/java/com/onezogreen/onezo3/sales/SalesService.java
@@ -3,21 +3,28 @@ package com.onezogreen.onezo3.sales;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class SalesService {
-    private final SalesMapper saleMapper;
+    private final SalesMapper salesMapper;
 
     public List<SaleVo> getSalesByStore(Long storeId) {
-        return saleMapper.getSalesByStore(storeId);
+        return salesMapper.getSalesByStore(storeId);
     }
 
     public List<SaleVo> getSalesByMenu(Long storeId, Long menuId) {
-        return saleMapper.getSalesByMenu(storeId, menuId);
+        return salesMapper.getSalesByMenu(storeId, menuId);
     }
 
     public List<SaleVo> getSalesByType(Long storeId, TakeInOut takeInOut) {
-        return saleMapper.getSalesByType(storeId, takeInOut);
+        return salesMapper.getSalesByType(storeId, takeInOut);
+    }
+
+    public Map<String, List<SaleVo>> getMonthlySales(Long storeId, String month) {
+        List<SaleVo> sales = salesMapper.getSalesByMonth(storeId, month);
+        return sales.stream().collect(Collectors.groupingBy(SaleVo::getSaleDateFormatted));
     }
 }
